61c505e0f4cd3ef13a0e85f1d8dd7d78558966d3
Felipe Nogueira <contato.fnog@gmail.com>
2019-11-21 11:54:36 -0300

    Update Resolucao.php

--- src/TextWrap/Resolucao.php
+++ src/TextWrap/Resolucao.php
@@ -11,7 +11,26 @@ class Resolucao implements TextWrapInterface {
    * {@inheritdoc}
    */
   public function textWrap(string $text, int $length): array {
-    return [""];
-  }
+    $array = explode(" ", $text);
+    $textWrapped[0] = '';
+    $chaveF = 0;
+      
+    foreach ($array as $chave => $valor) {
 
+      $tamanho = (strlen($textWrapped[$chaveF]) + strlen($valor));
+        
+      if ($tamanho <= $length) {
+        $textWrapped[$chaveF] .= $array[$chave];
+      } else {
+        $chaveF++;
+        $textWrapped[$chaveF] = $array[$chave];
+      }
+
+      $textWrapped[$chaveF] .= " ";
+      $textWrappedOutput = array_map(function($e) { return trim($e); }, $textWrapped);
+      
+    }
+    
+    return $textWrappedOutput;
+  }
 }
